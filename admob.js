06d6ba0d0fae790e4db348f09091e23d1069ac0a
kerblaster <kerblaster@hotmail.com>
2015-06-12 16:50:46 -0400

    admob

--- admob.js
+++ admob.js
@@ -2,36 +2,33 @@
 var admobid = {};
 
  function onLoadAd() {
-	alert("onLoadAd()");
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { // for Android
-			banner: 'ca-app-pub-6869992474017983/9375997553',
-			interstitial: 'ca-app-pub-6869992474017983/1657046752'
+			banner: 'ca-app-pub-6959590598551540/6197908715',
+			interstitial: 'ca-app-pub-6959590598551540/7535041114'
 		};
+		alert("onLoadAd(): android"); //debug
 	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
 		admobid = { // for iOS
-			banner: 'ca-app-pub-6869992474017983/4806197152',
-			interstitial: 'ca-app-pub-6869992474017983/7563979554'
+			banner: 'ca-app-pub-6959590598551540/9151375115',
+			interstitial: 'ca-app-pub-6959590598551540/9011774319'
 		};
+		alert("onLoadAd(): android");	//debug
 	} else {
 		admobid = { // for Windows Phone
 			banner: 'ca-app-pub-6869992474017983/8878394753',
-			interstitial: 'ca-app-pub-6869992474017983/1355127956'
+			interstitial: 'ca-app-pub-6959590598551540/1488507514'
 		};
 	}
 	
 	initApp();
 }
     
-function initApp() {
-	alert("initApp(): AdMob="+AdMob);
-	if (! AdMob ) { alert( 'Error loading Ads' ); return; }
-	alert( 'initApp()' );//debug
-	// display the banner at startup
-	AdMob.createBanner( {
-		adId:admobid.banner, 
-		adSize:'MEDIUM_RECTANGLE', 
-		overlap:true, 
-		position:AdMob.AD_POSITION.BOTTOM_CENTER, 
-		autoShow:true} );
+function initApp(){
+	alert("initApp()");
+	alert("AdMob="+AdMob);
+	if(AdMob) AdMob.createBanner( {
+		adId: admobid.banner, 
+		position: AdMob.AD_POSITION.BOTTOM_CENTER, 
+		autoShow: true } );
 }
