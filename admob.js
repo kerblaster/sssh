5e9f83cf77349383248a4fe9ea88db6876d1cf05
kerblaster <kerblaster@hotmail.com>
2015-06-12 16:35:40 -0400

    admob take 7

--- admob.js
+++ admob.js
@@ -1,6 +1,8 @@
+ 	
+var admobid = {};
+
  function onLoadAd() {
-	
-	var admobid = {};
+	alert("onLoadAd()");
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { // for Android
 			banner: 'ca-app-pub-6869992474017983/9375997553',
@@ -19,18 +21,17 @@
 	}
 	
 	initApp();
-		
-	function initApp() {
-		alert("AdMob="+AdMob);
-		if (! AdMob ) { alert( 'Error loading Ads' ); return; }
-		alert( 'initApp()' );//debug
-		// display the banner at startup
-		AdMob.createBanner( {
-			adId:admobid.banner, 
-			adSize:'MEDIUM_RECTANGLE', 
-			overlap:true, 
-			position:AdMob.AD_POSITION.BOTTOM_CENTER, 
-			autoShow:true} );
-	}
 }
     
+function initApp() {
+	alert("initApp(): AdMob="+AdMob);
+	if (! AdMob ) { alert( 'Error loading Ads' ); return; }
+	alert( 'initApp()' );//debug
+	// display the banner at startup
+	AdMob.createBanner( {
+		adId:admobid.banner, 
+		adSize:'MEDIUM_RECTANGLE', 
+		overlap:true, 
+		position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+		autoShow:true} );
+}
