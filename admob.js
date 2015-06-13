941bbb8387fe71ccf17fe56cd6d6fed145b8e1b0
kerblaster <kerblaster@hotmail.com>
2015-06-13 03:29:01 -0400

    ads work?

--- admob.js
+++ admob.js
@@ -6,13 +6,11 @@ var admobid = {};
 			banner: 'ca-app-pub-6959590598551540/6197908715',
 			interstitial: 'ca-app-pub-6959590598551540/7535041114'
 		};
-		alert("onLoadAd(): android"); //debug
 	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
 		admobid = { // for iOS
 			banner: 'ca-app-pub-6959590598551540/9151375115',
 			interstitial: 'ca-app-pub-6959590598551540/9011774319'
 		};
-		alert("onLoadAd(): android");	//debug
 	} else {
 		admobid = { // for Windows Phone
 			banner: 'ca-app-pub-6869992474017983/8878394753',
@@ -23,11 +21,10 @@ var admobid = {};
 	initApp();
 }
     
-function initApp(){
-	alert("initApp()");
-	alert("AdMob="+AdMob);
+function initApp(){	//delete adSize if there is a problem
 	if(AdMob) AdMob.createBanner( {
 		adId: admobid.banner, 
+		adSize:'CUSTOM',   height:50, width:320,
 		position: AdMob.AD_POSITION.BOTTOM_CENTER, 
 		autoShow: true } );
 }
